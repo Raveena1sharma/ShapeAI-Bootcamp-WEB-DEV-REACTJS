b642c06f550920f5bfd6a71a4659db5c3ff3ebe0
Raveena1sharma <86569192+Raveena1sharma@users.noreply.github.com>
2021-06-28 10:21:31 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Info()
+{
+  return(
+    <div className="note">
+      <h1> Javascript and Reactjs Bootcamp.</h1>
+      <p> An amazing bootcamp taken by Mr. Shaurya Sinha sir.
+         We learnt everything like HTML,Javascript and React.JS from scratch.</p>
+         </div>
+  );
+}
+export default Info;
